0f0151e6f91d53a2572f8c53830931406c4c16fd
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-01 16:23:17 -0400

    iterating contacts but it keepsmultiplying them ???

--- crm.rb
+++ crm.rb
@@ -7,20 +7,9 @@ require 'sinatra'
   end
 
   get '/contacts' do
-
-  actual_value = Contact.all
-
-  p actual_value
-
+    @contacts = Contact.all
+    erb :contacts
   end
-
-
-  Contact.create(first_name: "melina", last_name: "mehr", email: "melinamehr@msn.com", note: "wasap")
-  Contact.create(first_name: "zach", last_name: "van", email: "zachvan@gmail.com", note: "wasap part 2")
-  Contact.create(first_name: "dj jilly", last_name: "j", email: "djjillyj@msn.com", note: "wasap part 3")
-
-
-
 
 
 after do
