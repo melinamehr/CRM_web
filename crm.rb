5f22c2062088b61125c9ccd4d9ba45d3915acddd
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-02 13:46:39 -0400

    added styles and layout code structures

--- crm.rb
+++ crm.rb
@@ -19,6 +19,21 @@ require 'sinatra'
     erb :maline
   end
 
+
+  get '/contacts/:id' do
+  # params[:id]
+  # 1. retrieve the recipe from the database
+  #class method on recipe number, find that recipe numeber
+  #then render the recipe page
+    @contact = Contact.find(params[:id])
+  # 2. render a recipe page
+  #params is the way to access information given to u
+  #thru the url; to access the info in the url:
+  #localhost2342/recipes/2 <--- that 2
+    erb:recipe
+  end
+
+
 after do
     ActiveRecord::Base.connection.close
 end
